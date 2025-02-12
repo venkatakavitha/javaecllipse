0867294a03e0b57f94244f3d4628f6ed8fe4b560
22360013007 <mudiyamkavitha@gmail.com>
2025-02-12 07:13:11 +0530

    simple programs

---
+++ javaprograms/Object.java
@@ -0,0 +1,18 @@
+package javaprograms;
+
+public class Object {
+	  
+	void show()    
+	{    
+	System.out.println("Welcome to java program on object");    
+	}    
+	public static void main(String[] args)   
+	{    
+ 
+	Object obj = new Object();   
+
+	obj.show();    
+	}    
+	}
+
+
